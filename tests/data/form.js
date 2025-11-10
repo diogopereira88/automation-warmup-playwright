dc034ed6d7e877576c5fa0b4a352a44d64772bc4
diogopereira88 <diogo.pereira@mindera.com>
2025-11-10 18:56:42 +0000

    test: add data driven form tests (success/error)

---
+++ tests/data/form.js
@@ -0,0 +1,62 @@
+export const USERS = [
+    {
+      scenario: "Multiple Hobbies",
+      name: "Diogo",
+      email: "diogo@test.com",
+      password: "123123",
+      countryLable: "Portugal",
+      countryValue: "portugal",
+      gender: "Male",
+      genderValue: "male",
+      hobbies: ["Read books", "Travel", "Video Games", "Sports", "Movies", "Board Games",]
+    },
+    {
+      scenario: "No Hobbies",
+      name: "Vanessa",
+      email: "vanessa@test.com",
+      password: "1234",
+      countryLable: "Brazil",
+      countryValue: "brazil",
+      gender: "Female",
+      genderValue: "female",
+      hobbies: []
+    },
+    {
+      scenario: "Multiple Hobbies",
+      name: "Filipa",
+      email: "filipa@test.com",
+      password: "12321",
+      ccountryLable: "Canada",
+      countryValue: "canada",
+      gender: "Other",
+      genderValue: "other",
+      hobbies: ["Read books", "Travel", "Video Games"]
+    },
+    {
+      scenario: "Multiple Hobbies",
+      name: "Bruno",
+      email: "bruno@test.com",
+      password: "111222",
+      countryLable: "United States of America",
+      countryValue: "usa",
+      gender: "Male",
+      genderValue: "male",
+      hobbies: ["Read books"]
+    },
+    {
+      scenario: "Multiple Hobbies",
+      name: "Diana",
+      email: "diana@test.com",
+      password: "0987",
+      countryLable: "Mexico",
+      countryValue: "mexico",
+      gender: "Female",
+      genderValue: "female",
+      hobbies: ["Sports", "Movies"]
+    },
+  ];
+
+  export const FORM_MESSAGES = {
+    successTitle: "Success!",
+    successMessage: "The form has been submitted",
+  };
---
+++ tests/form.spec.js
@@ -0,0 +1,67 @@
+import { test, expect } from '@playwright/test';
+import { USERS, FORM_MESSAGES } from './data/form';
+
+test.describe('FORM', () => {
+
+    test.beforeEach( async ({ page }) => {
+        await page.goto('/form');
+    });
+
+    for (const user of USERS) {
+        const title = `${user.scenario} | ${user.name} | ${user.countryLabel} | ${user.gender}`;
+        
+        test(title, async ({ page }) => {
+            await test.step('Fill required fields', async () => {
+                await page.getByRole('textbox', { name: 'Name *' })
+                .fill(user.name);
+                await page.getByRole('textbox', { name: 'Email *' })
+                .fill(user.email);
+                await page.getByRole('textbox', { name: 'Password *' })
+                .fill(user.password);
+                await page.getByLabel('Country *')
+                .selectOption(user.countryValue);
+                await page.getByRole('radio', { name: user.gender, exact: true }).check();
+
+            });
+
+            await test.step('Select Hobbies', async () => {
+                if (user.hobbies != []) {
+                    for (const hobby of user.hobbies) {
+                        await page.getByRole('checkbox', { name: hobby }).check();
+                    }
+                }
+            });
+
+            await test.step ('Submit and validate success', async () => {
+                await page.getByRole('button', {name: 'Send'}).click();
+                await expect(page.getByText(FORM_MESSAGES.successTitle)).toBeVisible();
+                await expect(page.getByText(FORM_MESSAGES.successMessage)).toBeVisible();
+            });
+        });
+    }
+});
+
+test.describe('FORM - ERROR', () => {
+
+    test.beforeEach( async ({ page }) => {
+        await page.goto('/form');
+    });
+
+    test('Required validation messages', async ({ page }) => {
+        await test.step('Submit without filling anything', async () => {
+            await page.getByRole('button', { name: 'Send' }).click();
+        });
+        await test.step('Validate required messages are visible', async () => {
+            await expect(page.getByText('The name field is required.'))
+            .toBeVisible();
+            await expect(page.getByText('The email field is required.'))
+            .toBeVisible();
+            await expect(page.getByText('The password field is required.'))
+            .toBeVisible();
+            await expect(page.getByText('The country field is required.'))
+            .toBeVisible();
+            await expect(page.getByText('The gender field is required.'))
+            .toBeVisible();
+        });
+    });
+});
